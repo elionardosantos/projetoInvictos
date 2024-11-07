c3d54b131983337e5d6a721ceb18fcb79ebf640a
Elionardo <47214962+elionardosantos@users.noreply.github.com>
2024-11-07 16:51:43 +0000

    [em andamento] Implementando consulta de produto para o pedido

--- novo_pedido_process.php
+++ novo_pedido_process.php
@@ -82,15 +82,17 @@ function novoPedido(){
         "Accept: application/json",
         "authorization: bearer " . $token
     ];
-
+    
+    $listaProdutos = [30, 44];
     $postData = [
         "contato"=>[
             "id"=>$contatoId
         ],
         "itens"=>[
             //criando função para buscar id dos produtos. Linha 124
+            consultaProdutoId($listaProdutos)
         ],
-        "data"=>"2024-11-06"
+        "data"=>"2024-11-07"
     ];
 
     $jsonPostData = json_encode($postData);
@@ -121,9 +123,33 @@ function novoPedido(){
     }
 }
 // RETORNA O ID DOS PRODUTOS
-function(){
+function consultaProdutoId($listaProdutos){
+    $link = "";
+    foreach($listaProdutos as $produto){
+        $link .= "&codigos%5B%5D=$produto";
+    }
+    $url = "https://api.bling.com.br/Api/v3/produtos?$link";
+    $jsonFile = file_get_contents('config/token_request_response.json');
+    $jsonData = json_decode($jsonFile, true);
+    $token = isset($jsonData['access_token'])?$jsonData['access_token']:"";
+    $header = array(
+        "authorization: bearer " . $token
+    );
+    $cURL = curl_init($url);
+    curl_setopt($cURL, CURLOPT_URL, $url);
+    curl_setopt($cURL, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
 
+    $response = curl_exec($cURL);
+    $responseData = json_decode($response, true);
+    
+    // echo "<script>console.log($response)</script>";
+    foreach($responseData['data'] as $produto){
+        return $produto['id'].",";
+    }
 }
+
+
 // ################# FUNCTIONS END #########################
 
 
--- testes.php
+++ testes.php
@@ -1,17 +1,29 @@
 <?php
 
-$state = "SP";
+$listaProdutos = [30, 44];
+consultaProdutoId($listaProdutos);
+function consultaProdutoId($listaProdutos){
+    $link = "";
+    foreach($listaProdutos as $produto){
+        $link .= "&codigos%5B%5D=$produto";
+    }
+    $url = "https://api.bling.com.br/Api/v3/produtos?$link";
+    $jsonFile = file_get_contents('config/token_request_response.json');
+    $jsonData = json_decode($jsonFile, true);
+    $token = isset($jsonData['access_token'])?$jsonData['access_token']:"";
+    $header = array(
+        "authorization: bearer " . $token
+    );
+    $cURL = curl_init($url);
+    curl_setopt($cURL, CURLOPT_URL, $url);
+    curl_setopt($cURL, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
 
-$estados = [
-    "SP", "RJ", "MG", "ES", 
-    "AC", "AL", "AP", "AM", 
-    "BA", "CE", "DF", "GO", 
-    "MA", "MT", "MS", "PA", 
-    "PB", "PR", "PE", "PI", 
-    "RN", "RS", "RO", "RR", 
-    "SC", "SE", "TO"
-];
-
-foreach($estados as $uf){
-    echo "<option value=\"$uf\" <?php if(isset($state) && $state === '$uf') {echo 'selected';} ?>>$uf</option>\n";
+    $response = curl_exec($cURL);
+    $responseData = json_decode($response, true);
+    
+    // echo "<script>console.log($response)</script>";
+    foreach($responseData['data'] as $produto){
+        echo $produto['id'].",";
+    }
 }
