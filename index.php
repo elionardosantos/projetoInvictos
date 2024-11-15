a3c19ee07139fff94428bdf056d404688fca2643
Elionardo <47214962+elionardosantos@users.noreply.github.com>
2024-11-15 18:00:11 +0000

    Ajustes na tela inicial

--- index.php
+++ index.php
@@ -11,15 +11,17 @@
 
     ?>
     <div class="container my-3">
-        <h2>Início</h2>
-        <p>
-        <?php
-            date_default_timezone_set('America/Sao_Paulo');
-            echo $dataHoraAtual = date('d/m/Y');
-        ?>
-        </p>
-
-        <!-- <div id="ww_f400bb6604242" v='1.3' loc='auto' a='{"t":"responsive","lang":"pt","sl_lpl":1,"ids":[],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"#FFFFFF00","cl_font":"#000000","cl_cloud":"#d4d4d4","cl_persp":"#2196F3","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722","cl_odd":"#00000000"}'>Mais previsões: <a href="https://oneweather.org/pt/lisbon/25_days/" id="ww_f400bb6604242_u" target="_blank">Meteorologia 25 dias</a></div><script async src="https://app3.weatherwidget.org/js/?id=ww_f400bb6604242"></script> -->
+        <h2>
+            <?php
+                date_default_timezone_set('America/Sao_Paulo');
+                echo $dataHoraAtual = date('d/m/Y');
+            ?>
+        </h2>
     </div>
+    <div class="container my-3">
+        <a href="novo_orcamento.php" class="btn btn-primary" role="button">Novo Orçamento</a>
+        <a href="consulta_cnpj_visualizacao.php" class="btn btn-primary" role="button">Consultar CNPJ</a>
+    </div>
+    <!-- <div id="ww_f400bb6604242" v='1.3' loc='auto' a='{"t":"responsive","lang":"pt","sl_lpl":1,"ids":[],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"#FFFFFF00","cl_font":"#000000","cl_cloud":"#d4d4d4","cl_persp":"#2196F3","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722","cl_odd":"#00000000"}'>Mais previsões: <a href="https://oneweather.org/pt/lisbon/25_days/" id="ww_f400bb6604242_u" target="_blank">Meteorologia 25 dias</a></div><script async src="https://app3.weatherwidget.org/js/?id=ww_f400bb6604242"></script> -->
 </body>
 </html>
